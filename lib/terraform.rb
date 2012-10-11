62dccc5d49bc0867051abccf84564214b42e56ce
Daniel MacDougall <dmacdougall@gmail.com>
2012-10-11 14:53:39 -0700

    Add plugin registration and writing all plugin files

--- lib/terraform.rb
+++ lib/terraform.rb
@@ -2,9 +2,21 @@ require "terraform/version"
 require "fileutils"
 
 module Terraform
+  # DEPRECATED
+  # Replaced by `write_dsl_files`. Remove in next major version release.
   # Writes the terraform_dsl.rb to the given file or directory.
   def self.write_dsl_file(path)
     path = File.join(path, "terraform_dsl.rb") if File.directory?(path)
     FileUtils.cp(File.expand_path(File.join(File.dirname(__FILE__), "terraform/dsl.rb")), path)
+  end
+
+  @@plugin_files = Set.new [File.expand_path(File.join(File.dirname(__FILE__), "terraform/dsl.rb"))]
+  def self.register_plugin(path) @@plugin_files.add(path) end
+
+  def self.write_dsl_files(path)
+    FileUtils.mkdir_p(path)
+    @@plugin_files.each do |plugin|
+      FileUtils.cp(File.expand_path(plugin), File.join(path, File.basename(plugin)))
+    end
   end
 end
